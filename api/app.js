fad991f1e6d5d809197f26a6a6388e7938db5c85
Dmytro Yemelianov <6231326+miwaniza@users.noreply.github.com>
2023-06-05 22:21:30 +0300

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,16 +1,4 @@
-// Airtable API configuration
-const apiKey = process.env.AIRTABLE_APIKEY;
-const baseId = process.env.AIRTABLE_BASEID;
-const tableName = process.env.AIRTABLE_TABLEID;
-
-// Fetch image data from Airtable
-async function fetchImageData() {
-  const response = await fetch(
-    `https://api.airtable.com/v0/${baseId}/${tableName}?api_key=${apiKey}`
-  );
-  const data = await response.json();
-  return data.records;
-}
+// main.js
 
 // Display image and description
 function displayCard(imageUrl, description) {
@@ -21,65 +9,42 @@ function displayCard(imageUrl, description) {
   descriptionElement.textContent = description;
 }
 
+// Fetch image data from the serverless function
+async function fetchImageData() {
+  const response = await fetch('/api');
+  const data = await response.json();
+  return data;
+}
+
 // Initialize the app
 async function initApp() {
-  const imageData = await fetchImageData();
-  let currentIndex = 0;
+  try {
+    const imageData = await fetchImageData();
+    let currentIndex = 0;
 
-  function loadNextCard() {
-    if (currentIndex >= imageData.length) {
-      alert('No more cards!');
-      return;
+    function loadNextCard() {
+      if (currentIndex >= imageData.length) {
+        alert('No more cards!');
+        return;
+      }
+
+      const { url, description } = imageData[currentIndex];
+      displayCard(url, description);
+      currentIndex++;
     }
 
-    const { url, description } = imageData[currentIndex].fields;
-    displayCard(url, description);
-    currentIndex++;
-  }
-
-  // Handle swipe actions (like/dislike)
-  function handleSwipeAction(swipedRight) {
-    // You can perform any action here based on the swipe direction (like/dislike)
-    // For this example, we'll just log the action to the console.
-    console.log(swipedRight ? 'Liked' : 'Disliked');
-
-    // Save the swipe action to Airtable
-    const ratingsTable = process.env.AIRTABLE_RATINGS_TABLEID;
-    fetch(`https://api.airtable.com/v0/${baseId}/${ratingsTable}`, {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${apiKey}`,
-        'Content-Type': 'application/json'
-      },
-      // Save the swipe action to Airtable
-      body: JSON.stringify({
-        records: [
-          {
-            fields: {
-              // Save the image ID to Airtable
-              image: [imageData[currentIndex - 1].id],
-              // Save the swipe direction (like/dislike) to Airtable
-              liked: swipedRight
-            }
-          }
-        ]
-      })
-    });
-
-  
-    // Load the next card
     loadNextCard();
+
+    // Add event listeners to swipe buttons
+    document.getElementById('like-button').addEventListener('click', () => handleSwipeAction(true));
+    document.getElementById('dislike-button').addEventListener('click', () => handleSwipeAction(false));
+
+    // Load the next card when a swipe action occurs
+    document.getElementById('card').addEventListener('transitionend', loadNextCard);
+  } catch (error) {
+    console.error(error);
+    alert('Failed to fetch image data');
   }
-
-  loadNextCard();
-
-  // Add event listeners to swipe buttons
-  document.getElementById('like-button').addEventListener('click', () =>
-    handleSwipeAction(true)
-  );
-  document.getElementById('dislike-button').addEventListener('click', () =>
-    handleSwipeAction(false)
-  );
 }
 
 // Start the app
